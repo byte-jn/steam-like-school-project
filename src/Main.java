8466c32be033ac14c8f95221c8ec636cf0888689
Jannis Lauer <j.lauer@elio-systems.com>
2026-03-17 11:05:08 +0100

    clean up

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        FunctionService session = new FunctionService();
-
-        User user = session.initializeUser();
-    }
-}
